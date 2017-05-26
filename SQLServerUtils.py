eee490b8634b74903cf4ab4b6966330629ffa05a
Carole <cmshaw@fredhutch.org>
2017-05-26 07:00:38 -0700

    minor

--- SQLServerUtils.py
+++ SQLServerUtils.py
@@ -1,4 +1,5 @@
 import pypyodbc
+print(pypyodbc.drivers())
 
 def connect_to_caisisro():
     try:
@@ -14,17 +15,17 @@ def connect_to_caisisro():
                                'Trusted_Connection=yes'
                                'uid=fhcrc\cmshaw;pwd=password'
                                )
-        con = pypyodbc.connect('Driver = (SQL Server);'
-                               'Server=CONGO-H\H;'
-                               'Database=CaisisProd;'
-                               'Trusted_Connection=yes'
-                               'uid=fhcrc\cmshaw;pwd=Aaron!23'
+        con = pypyodbc.connect('DRIVER={ODBC Driver 13 for SQL Server};'
+                               'SERVER=CONGO-H\H;'
+                               'DATABASE=CaisisProd;'
+                               'UID=fhcrc\cmshaw;'
+                               'PWD=Aaron!23'
                                )
         print ('Connected')
         print ('Connected')
         con.close()
     except Exception as ErrVal:
-        print ('Connection Failed')
+        print ('\nConnection Failed')
         print (ErrVal)
     return None
 
--- SQLServer_pyodbc.py
+++ SQLServer_pyodbc.py
@@ -3,13 +3,17 @@ import pyodbc
 
 def connect_to_caisisprod():
     try:
-        con = pyodbc.connect(r'Driver = {ODBC Driver 11 for SQL Server};'
-           r'Server=CONGO-H\H;'
-           r'Database=CaisisProd;'
-           r'uid=fhcrc\cmshaw;pwd=Aaron!23'
+        # con = pyodbc.connect(r'DRIVER={SQL Server};'
+        con = pyodbc.connect(r'DRIVER={ODBC Driver 13 for SQL Server};'
+           r'SERVER=CONGO-H\H;'
+           r'DATABASE=CaisisProd;'
+           r'UID=fhcrc/cmshaw;'
+           r'PWD=Aaron!23'
            )
+        con.close()
     except Exception as ErrVal:
         print ('Connection Failed')
         print (ErrVal)
 
+print(pyodbc.drivers())
 connect_to_caisisprod()
