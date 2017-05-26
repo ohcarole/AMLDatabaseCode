a680b5dc0cb2e2a38bf338a0c03d0212e16b25ca
Carole <cmshaw@fredhutch.org>
2017-05-26 13:56:26 -0700

    minor

--- SQLServer_pyodbc.py
+++ SQLServer_pyodbc.py
@@ -14,7 +14,31 @@ def connect_to_caisisprod():
         print (ErrVal)
     return con
 
+
+def get_cnxdict_(sect):
+    cnxdict = {
+        'desc': 'connection and cursor information'
+        , 'ini_section': sect
+        , 'ini_file': 'J:\Estey_AML\AML Programming\Python\sharedUtils\Config.ini'
+        , 'out_filedir': ''
+        , 'out_filename': ''
+        , 'out_fileext': ''
+        , 'out_filepath': ''  # this is built from the other fields
+        , 'schema': ''
+        , 'tablelist': []
+        , 'currtable': ''
+        , 'myconfig': ''
+        , 'itemnum': 0
+        , 'cnx': {}
+        , 'crs': {}
+        , 'sql': ''
+        , 'df': {}
+        , 'multi': False
+    }
+    return cnxdict
+
+cnxdict = get_cnxdict('caisisprod')
+cnxdict = read_db_config(cnxdict)
 print(pyodbc.drivers())
 con = connect_to_caisisprod()
-cnxdict = get_cnxdict('')
 con.close()
