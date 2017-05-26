3ddbe7ce9358b4333f9b0f57e39a13dfacce2207
Carole <cmshaw@fredhutch.org>
2017-05-25 20:48:13 -0700

    minor

--- SQLServer_pyodbc.py
+++ SQLServer_pyodbc.py
@@ -1,12 +1,15 @@
 import pyodbc
 
 
-
-def connect_to_caisisro():
+def connect_to_caisisprod():
     try:
-        pass
+        con = pyodbc.connect('Driver = (SQL Server);'
+                               'Server=CONGO-H\H;'
+                               'Database=CaisisProd;'
+                               'uid=fhcrc\cmshaw;pwd=Aaron!23'
+                               )
     except Exception as ErrVal:
         print ('Connection Failed')
         print (ErrVal)
 
-connect_to_caisisro()
+connect_to_caisisprod()
